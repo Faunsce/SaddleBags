e9b804bf3eacaa3af18d7c6ce1f7f5d11a3459f3
Dominik Rutter <dominik.rutter@gmail.com>
2023-04-12 16:42:13 -0400

    Introduce bare minimum functionality to program, all of this is test code and will not survive

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,30 @@
 ﻿#include <iostream>
 
+// Use a custom namespace for personally created functions
+namespace anna {
+	// Declare function for testing input
+	void testFunc(int);
+}
+
 int main()
 {
-	std::cout << "Hello CMake." << std::endl;
+	// Create place in memory to store user input
+	int num;
+
+	// Request and process user input
+	std::cout << "Please enter a number : ";
+	std::cin >> num;
+	std::cout << std::endl;
+
+	// Make use of user input data
+	anna::testFunc(num);
+
 	return 0;
+}
+
+namespace anna {
+	// Create function definition so it has functionality
+	void testFunc(int num) {
+		std::cout << "The number you input was : " << num << std::endl;
+	}
 }
