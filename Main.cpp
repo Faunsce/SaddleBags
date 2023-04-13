44070717ca30237bd30d021fe1ba9efed56f60b5
Dominik Rutter <dominik.rutter@gmail.com>
2023-04-13 19:32:37 -0400

    Ability to handle floats introduced

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,14 @@
 // Use a custom namespace for personally created functions
 namespace anna {
 	// Declare function for testing input
-	void testFunc(int);
+	void testFunc(float);
 	bool integerCheck(std::string);
 }
 
 int main()
 {
 	// Create place in memory to store user input
-	long int num = 0; // 8 bit Integer
+	float num = 0; // 8 bit float
 	std::pair<std::string, bool> userInput{"", false}; // Store data alongside a "clean" check
 
 	// Request and process user input
@@ -37,7 +37,7 @@ int main()
 			std::cout << ", please try again." << std::endl << std::endl;
 		} else {
 			std::cout << "Input accepted!" << std::endl;
-			num = std::stoi(userInput.first);
+			num = std::stof(userInput.first);
 		}
 
 	} while (!userInput.second); // Check if data is clean, otherwise have the user re-enter their input
@@ -50,11 +50,12 @@ int main()
 
 namespace anna {
 	// Create function definition so it has functionality
-	void testFunc(int num) {
+	void testFunc(float num) {
 		std::cout << "The number you input was : " << num << std::endl;
 	}
 
 	bool integerCheck(std::string data) {
+		bool isFloat = false;
 		if (data[0] == '-') {
 			if (data.length() > 1) {
 				data = data.erase(0, 1);
@@ -63,6 +64,14 @@ namespace anna {
 			}
 		}
 		for (auto&& slot : data) {
+			if (slot == '.') { // Check for decimals, more than one is improper formatting and fails the check
+				if (!isFloat) {
+					isFloat = true; 
+					continue; // Prevent num check from failing on first decimal
+				} else {
+					return false; // More than one decimal point in number
+				}
+			}
 			if (!(slot >= '0' && slot <= '9')) {
 				return false; // Data unclean
 			}
@@ -74,7 +83,6 @@ namespace anna {
 // Notes
 
 /*
-- Add the ability to capture negative values
 - Add the ability to input a float and report if the number is a float vs int and pos vs neg
 - Create print function for easy printing with optional arguments for formatting
 */
