87eb4397b0040d5a66c9f95716d1ffcecee599aa
Dominik Rutter <dominik.rutter@gmail.com>
2023-04-14 20:19:55 -0400

    General code refactor, same functionality as before.
    - Also added more return info such as informing the user that their value was pos vs neg or float vs int

--- Main.cpp
+++ Main.cpp
@@ -3,80 +3,83 @@
 
 // Use a custom namespace for personally created functions
 namespace anna {
+	// Declare types
+	struct flags
+	{
+		bool clean; 
+		bool sign; // The "Polarity" so to speak, of the value
+		bool whole; // If the number is "whole" aka float vs int
+		
+	};
 	// Declare function for testing input
-	void testFunc(float);
-	bool integerCheck(std::string);
+	anna::flags inputCheck(std::string);
 }
 
 int main()
 {
 	// Create place in memory to store user input
-	float num = 0; // 8 bit float
-	std::pair<std::string, bool> userInput{"", false}; // Store data alongside a "clean" check
+	std::string userInput; // Store user input
 
 	// Request and process user input
+	anna::flags returnedData;
 	do {
 		std::cout << "Please enter a number : ";
-		std::getline(std::cin, userInput.first);
+		std::getline(std::cin, userInput);
 		std::cin.clear();
 
-		userInput.second = true;
-		
-		if (userInput.first.empty()) { // Check if the input had any data
-			std::cout << "No data input";
-			userInput.second = false;
-		} else if (userInput.first.length() > 10) { // Check that the input is no less than 10 digits
-			std::cout << "Number must be smaller than digits in length";
-			userInput.second = false;
-		} else if (!anna::integerCheck(userInput.first)) { //  Check that the input is an integer
-			std::cout << "Input must be an integer";
-			userInput.second = false;
-		}
+		returnedData = anna::inputCheck(userInput);
 
-		if (!userInput.second) { // If data is unclean, finish warn and then try again
-			std::cout << ", please try again." << std::endl << std::endl;
-		} else {
-			std::cout << "Input accepted!" << std::endl;
-			num = std::stof(userInput.first);
-		}
-
-	} while (!userInput.second); // Check if data is clean, otherwise have the user re-enter their input
+	} while (!returnedData.clean); // Check if data is clean, otherwise have the user re-enter their input
 
 	// Make use of user input data
-	anna::testFunc(num);
+	std::cout << "Your value is : " << userInput << std::endl
+		<< "You had a " << ((returnedData.sign) ? "positive " : "negative ") << ((returnedData.whole) ? "integer " : "float ");
 
 	return 0;
 }
 
 namespace anna {
 	// Create function definition so it has functionality
-	void testFunc(float num) {
-		std::cout << "The number you input was : " << num << std::endl;
-	}
-
-	bool integerCheck(std::string data) {
-		bool isFloat = false;
-		if (data[0] == '-') {
-			if (data.length() > 1) {
-				data = data.erase(0, 1);
-			} else {
-				return false;
-			}
-		}
-		for (auto&& slot : data) {
-			if (slot == '.') { // Check for decimals, more than one is improper formatting and fails the check
-				if (!isFloat) {
-					isFloat = true; 
-					continue; // Prevent num check from failing on first decimal
-				} else {
-					return false; // More than one decimal point in number
+	anna::flags inputCheck(std::string data) {
+		anna::flags dataFlags = anna::flags{ false, true, true };
+		
+		if (data.length() > 0) { // Ensure input isn't empty
+			if (data.length() < 10) { // Ensure input isn't too long
+				if (data[0] == '-') { // Check for negative input
+					if (data.length() > 1 && (data[1] > '0' && data[1] < '9')) { // Ensure there is data after the sign
+						data.erase(0, 1);
+						dataFlags.sign = false;
+					} else {
+						std::cout << "Input must contain data after sign of number" << std::endl;
+						return dataFlags;
+					}
+				} 
+				for (auto&& pos : data) { // Scan string for decimals and numbers, fail on non decimal and numbers
+					if (pos == '.') {
+						if (dataFlags.whole == true) {
+							dataFlags.whole = false;
+							continue; // Keep num check from failing first decimal
+						} else {
+							std::cout << "Input may not have more than one decimal indicator, please try again!" << std::endl;
+							return dataFlags;
+						}
+					} else {
+						if (!(pos > '0' && pos < '9')) {
+							std::cout << "Only numbers, decimals, and a sign indicator are allowed, please try again!" << std::endl;
+							return dataFlags;
+						} 
+					}
 				}
+				dataFlags.clean = true;
+				return dataFlags; // Data clean
+			} else {
+				std::cout << "Data must be no longer than 10 characters, please try again!" << std::endl;
+				return dataFlags;
 			}
-			if (!(slot >= '0' && slot <= '9')) {
-				return false; // Data unclean
-			}
+		} else {
+			std::cout << "No data input, please try again!" << std::endl;
+			return dataFlags;
 		}
-		return true; // Data clean
 	}
 }
 
@@ -85,4 +88,5 @@ namespace anna {
 /*
 - Add the ability to input a float and report if the number is a float vs int and pos vs neg
 - Create print function for easy printing with optional arguments for formatting
+- Add ability for program to loop, give ability to exit without killing window.
 */
